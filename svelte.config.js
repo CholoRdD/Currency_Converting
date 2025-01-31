5169aa059230c3d55c05f51d09a8a876e5ae4809
Poncholo Riego de Dios <poncholoriegodedios@gmail.com>
2025-01-31 16:47:32 +0800

    tweaking svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -3,12 +3,12 @@ import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production'?'/Currency_Converting':'',
+			base: process.env.GITHUB ? '/Currency_Converting' : ''
+		},
+		prerender: {
+			entries: ['*']  // Ensures all pages are pre-rendered for static hosting
 		}
 	}
 };
